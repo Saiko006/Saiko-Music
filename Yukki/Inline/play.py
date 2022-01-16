c3f04e0d1705fa06575e09040fe63c81657d253a
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-16 09:35:30 +0700

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -65,21 +65,21 @@ def search_markup(
     buttons = [
         [
             InlineKeyboardButton(
-                text="1️⃣", callback_data=f"Yukki {ID1}|{duration1}|{user_id}"
+                text="1️⃣", callback_data=f"tomi {ID1}|{duration1}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="2️⃣", callback_data=f"Yukki {ID2}|{duration2}|{user_id}"
+                text="2️⃣", callback_data=f"tomi {ID2}|{duration2}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="3️⃣", callback_data=f"Yukki {ID3}|{duration3}|{user_id}"
+                text="3️⃣", callback_data=f"tomi {ID3}|{duration3}|{user_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="4️⃣", callback_data=f"Yukki {ID4}|{duration4}|{user_id}"
+                text="4️⃣", callback_data=f"tomi {ID4}|{duration4}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="5️⃣", callback_data=f"Yukki {ID5}|{duration5}|{user_id}"
+                text="5️⃣", callback_data=f"tomi {ID5}|{duration5}|{user_id}"
             ),
         ],
         [
@@ -115,25 +115,25 @@ def search_markup2(
         [
             InlineKeyboardButton(
                 text="6️⃣",
-                callback_data=f"Yukki {ID6}|{duration6}|{user_id}",
+                callback_data=f"tomi {ID6}|{duration6}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="7️⃣",
-                callback_data=f"Yukki {ID7}|{duration7}|{user_id}",
+                callback_data=f"tomi {ID7}|{duration7}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="8️⃣",
-                callback_data=f"Yukki {ID8}|{duration8}|{user_id}",
+                callback_data=f"tomi {ID8}|{duration8}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text="9️⃣",
-                callback_data=f"Yukki {ID9}|{duration9}|{user_id}",
+                callback_data=f"tomi {ID9}|{duration9}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="🔟",
-                callback_data=f"Yukki {ID10}|{duration10}|{user_id}",
+                callback_data=f"tomi {ID10}|{duration10}|{user_id}",
             ),
         ],
         [
