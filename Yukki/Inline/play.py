bed0d6d7721dd3967615837800e5e3ff451e26ef
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-16 11:40:38 +0700

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -151,6 +151,109 @@ def search_markup2(
     return buttons
 
 
+def search_markup3(
+    ID1,
+    ID2,
+    ID3,
+    ID4,
+    ID5,
+    duration1,
+    duration2,
+    duration3,
+    duration4,
+    duration5,
+    user_id,
+    query,
+):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="1️⃣", callback_data=f"Yukki {ID1}|{duration1}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="2️⃣", callback_data=f"Yukki {ID2}|{duration2}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="3️⃣", callback_data=f"Yukki {ID3}|{duration3}|{user_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="4️⃣", callback_data=f"Yukki {ID4}|{duration4}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="5️⃣", callback_data=f"Yukki {ID5}|{duration5}|{user_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="<", callback_data=f"popat 1|{query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="🗑 Close", callback_data=f"forceclose {query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text=">", callback_data=f"popat 1|{query}|{user_id}"
+            ),
+        ],
+    ]
+    return buttons
+
+
+def search_markup4(
+    ID6,
+    ID7,
+    ID8,
+    ID9,
+    ID10,
+    duration6,
+    duration7,
+    duration8,
+    duration9,
+    duration10,
+    user_id,
+    query,
+):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="6️⃣",
+                callback_data=f"Yukki {ID6}|{duration6}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="7️⃣",
+                callback_data=f"Yukki {ID7}|{duration7}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="8️⃣",
+                callback_data=f"Yukki {ID8}|{duration8}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="9️⃣",
+                callback_data=f"Yukki {ID9}|{duration9}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="🔟",
+                callback_data=f"Yukki {ID10}|{duration10}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="<", callback_data=f"popat 2|{query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="🗑 Close", callback_data=f"forceclose {query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text=">", callback_data=f"popat 2|{query}|{user_id}"
+            ),
+        ],
+    ]
+    return buttons
+
+
 def secondary_markup(videoid, user_id):
     buttons = [
         [
