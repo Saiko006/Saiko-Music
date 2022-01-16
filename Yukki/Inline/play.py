50d99479fe185285c6ae33a102a11fa0c7c74f8e
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-16 13:33:53 +0700

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -65,21 +65,21 @@ def search_markup(
     buttons = [
         [
             InlineKeyboardButton(
-                text="1️⃣", callback_data=f"Choose {ID1}|{duration1}|{user_id}"
+                text="1️⃣", callback_data=f"VideoStream {ID1}|{duration1}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="2️⃣", callback_data=f"Choose {ID2}|{duration2}|{user_id}"
+                text="2️⃣", callback_data=f"VideoStream {ID2}|{duration2}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="3️⃣", callback_data=f"Choose {ID3}|{duration3}|{user_id}"
+                text="3️⃣", callback_data=f"VideoStream {ID3}|{duration3}|{user_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="4️⃣", callback_data=f"Choose {ID4}|{duration4}|{user_id}"
+                text="4️⃣", callback_data=f"VideoStream {ID4}|{duration4}|{user_id}"
             ),
             InlineKeyboardButton(
-                text="5️⃣", callback_data=f"Choose {ID5}|{duration5}|{user_id}"
+                text="5️⃣", callback_data=f"VideoStream {ID5}|{duration5}|{user_id}"
             ),
         ],
         [
@@ -115,25 +115,25 @@ def search_markup2(
         [
             InlineKeyboardButton(
                 text="6️⃣",
-                callback_data=f"Choose {ID6}|{duration6}|{user_id}",
+                callback_data=f"VideoStream {ID6}|{duration6}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="7️⃣",
-                callback_data=f"Choose {ID7}|{duration7}|{user_id}",
+                callback_data=f"VideoStream {ID7}|{duration7}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="8️⃣",
-                callback_data=f"Choose {ID8}|{duration8}|{user_id}",
+                callback_data=f"VideoStream {ID8}|{duration8}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
                 text="9️⃣",
-                callback_data=f"Choose {ID9}|{duration9}|{user_id}",
+                callback_data=f"VideoStream {ID9}|{duration9}|{user_id}",
             ),
             InlineKeyboardButton(
                 text="🔟",
-                callback_data=f"Choose {ID10}|{duration10}|{user_id}",
+                callback_data=f"VideoStream {ID10}|{duration10}|{user_id}",
             ),
         ],
         [
