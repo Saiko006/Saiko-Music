427d0e24b3ca2c0db81601f7111401541c797f9c
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-16 10:40:58 +0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -35,7 +35,7 @@ loop = asyncio.get_event_loop()
       
       
 @app.on_message(
-    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["vplay", f"play@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @PermissionCheck
