a3d2b3362e1750794021b92add323e0f89066233
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-16 23:48:42 +0700

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -20,6 +20,7 @@ def url_markup(videoid, duration, user_id):
             InlineKeyboardButton(
                 text="<", callback_data=f"popat 1|{query}|{user_id}"
             ),
+        ],
         [
             InlineKeyboardButton(
                 text="🗑 Close Search",
