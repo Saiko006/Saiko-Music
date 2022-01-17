c4366df33ba388596e11a82e0937ba8cbee0987e
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-18 00:44:54 +0700

    Update Callback.py

--- Yukki/Plugins/Callback.py
+++ Yukki/Plugins/Callback.py
@@ -641,6 +641,19 @@ async def down_playlisyts(_, CallbackQuery):
     await CallbackQuery.edit_message_reply_markup(
         reply_markup=InlineKeyboardMarkup(buttons)
     )
+    
+    
+@app.on_callback_query(filters.regex(pattern=r"good"))
+async def good(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = download_markup(videoid, user_id)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
 
 # callback memek
     
@@ -673,15 +686,15 @@ async def kontolanjing(_, CallbackQuery):
     user_name = CallbackQuery.from_user.first_name
     rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
     await CallbackQuery.edit_message_text(f"""
-    ✪ *👋 Hello {rpk} !*
-    ✪ ᴍʏ ɴᴀᴍᴇ ɪs ᴍᴜꜱɪᴄ ᴋᴇᴋɪɴɪᴀɴ [🤖](https://telegra.ph/file/ee46c0aba5c12e0d2bb71.jpg)
-    ✪ ɪ'ᴍ ᴀ ᴋᴇᴋɪɴɪᴀɴ ᴛʜᴇᴍᴇ ʙᴏᴛ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴜsɪᴄ & ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴜsᴇꜰᴜʟ ꜰᴇᴀᴛᴜʀᴇs!
-    ✪ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ [ᴢᴀʟ](https://t.me/rumahakhirat)
-    ────────────────────────
-    ✪ *ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /*
-    ────────────────────────
-    ✪ *Pᴏᴡᴇʀᴇᴅ 🔰 Bʏ: ᴍᴜꜱɪᴄ ᴋᴇᴋɪɴɪᴀɴ!*
-    """,
+✪ *👋 Hello {rpk} !*
+✪ ᴍʏ ɴᴀᴍᴇ ɪs ᴍᴜꜱɪᴄ ᴋᴇᴋɪɴɪᴀɴ [🤖](https://telegra.ph/file/ee46c0aba5c12e0d2bb71.jpg)
+✪ ɪ'ᴍ ᴀ ᴋᴇᴋɪɴɪᴀɴ ᴛʜᴇᴍᴇ ʙᴏᴛ ᴀ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴜsɪᴄ & ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴜsᴇꜰᴜʟ ꜰᴇᴀᴛᴜʀᴇs!
+✪ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ [ᴢᴀʟ](https://t.me/rumahakhirat)
+────────────────────────
+✪ *ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /*
+────────────────────────
+✪ *Pᴏᴡᴇʀᴇᴅ 🔰 Bʏ: ᴍᴜꜱɪᴄ ᴋᴇᴋɪɴɪᴀɴ!*
+""",
        disable_web_page_preview=True,
        reply_markup=InlineKeyboardMarkup(
         [
@@ -715,17 +728,4 @@ async def kontolanjing(_, CallbackQuery):
             ],
         ]
     )
-                                          
-  
-                                          
-@app.on_callback_query(filters.regex(pattern=r"good"))
-async def good(_, CallbackQuery):
-    await CallbackQuery.answer()
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    userid = CallbackQuery.from_user.id
-    videoid, user_id = callback_request.split("|")
-    buttons = download_markup(videoid, user_id)
-    await CallbackQuery.edit_message_reply_markup(
-        reply_markup=InlineKeyboardMarkup(buttons)
-    )
+
