aac99eb110aa8691bcb6ca8f3529bbdf1c909404
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-17 16:56:48 +0700

    Update stream.py

--- Yukki/Utilities/stream.py
+++ Yukki/Utilities/stream.py
@@ -83,7 +83,7 @@ async def start_stream(
             videoid, CallbackQuery.from_user.id, duration_min, duration_min
         )
         await mystic.delete()
-        cap:f"🏷️<b>__Judul__:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⏱<b>__Durasi:__</b> {duration_min} \n💡<b>__Info:__</b> [Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{videoid}) \n🎧**__Atas permintaan:__** {CallbackQuery.from_user.mention}"
+        cap = f"🏷️<b>__Judul__:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⏱<b>__Durasi:__</b> {duration_min} \n💡<b>__Info:__</b> [Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{videoid}) \n🎧**__Atas permintaan:__** {CallbackQuery.from_user.mention}"
         final_output = await CallbackQuery.message.reply_photo(
             photo=thumb,
             reply_markup=InlineKeyboardMarkup(buttons),
