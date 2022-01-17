a7803f856ab86c31e5c8263bfee156793bbd82f4
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-17 21:49:17 +0700

    Update Playlist.py

--- Yukki/Plugins/Playlist.py
+++ Yukki/Plugins/Playlist.py
@@ -15,7 +15,7 @@ from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
 
-__MODULE__ = "Playlist"
+__MODULE__ = "ᴘʟᴀʏʟɪsᴛ"
 __HELP__ = """
 
 
