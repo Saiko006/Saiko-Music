f138e16f75dd303a0e4b576ea974c503a0565722
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-18 09:39:11 +0700

    Update assistant.py

--- Yukki/Decorators/assistant.py
+++ Yukki/Decorators/assistant.py
@@ -11,31 +11,6 @@ from Yukki.Database import get_assistant, save_assistant
 from Yukki.Utilities.assistant import get_assistant_details
 
 
-@app.on_callback_query(filters.regex("unban_assistant"))
-async def unban_assistant_(_, CallbackQuery):
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    query, user_id = callback_request.split("|")
-    a = await app.get_chat_member(CallbackQuery.message.chat.id, BOT_ID)
-    if not a.can_restrict_members:
-        return await CallbackQuery.answer(
-            "I am not having ban/unban user permission. Ask any admin to unban the assistant.",
-            show_alert=True,
-        )
-    else:
-        try:
-            await app.unban_chat_member(
-                CallbackQuery.message.chat.id, user_id
-            )
-        except:
-            return await CallbackQuery.answer(
-                "Failed to unban",
-                show_alert=True,
-            )
-        return await CallbackQuery.edit_message_text(
-            "Assistant Unbanned. Try Playing Now."
-        )
-
 
 def AssistantAdd(mystic):
     async def wrapper(_, message):
@@ -58,58 +33,36 @@ def AssistantAdd(mystic):
             ran_ass
         )
         try:
-            b = await app.get_chat_member(message.chat.id, ASS_ID)
-            key = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="🗑 Unban Assistant",
-                            callback_data=f"unban_assistant a|{ASS_ID}",
-                        )
-                    ],
-                ]
-            )
-            if b.status == "kicked":
-                return await message.reply_text(
-                    f"Assistant Account[{ASS_ID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASS_USERNAME}",
-                    reply_markup=key,
-                )
+            b = await app.get_chat_member(message.chat.id, ASS_ID)      
             if b.status == "banned":
-                return await message.reply_text(
-                    f"Assistant Account[{ASS_ID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASS_USERNAME}",
-                    reply_markup=key,
-                )
+                await app.unban_chat_member(message.chat.id, ubot.id)
+            invite_link = await app.export_chat_invite_link(message.chat.id)
+            if "+" in invite_link:
+                invite = (invite_link.replace("+", "")).split("t.me/")[1]
+                link_invite = f"https://t.me/joinchat/{invite}"
+            await ASS_ACC.join_chat(link_invite)
+            await app.send_message(
+                message.chat.id,
+                f"{ubot.first_name} Berhasil Bergabung",
+            )
         except UserNotParticipant:
-            if message.chat.username:
-                try:
-                    await ASS_ACC.join_chat(message.chat.username)
-                except UserAlreadyParticipant:
-                    pass
-                except Exception as e:
-                    await message.reply_text(
-                        f"__Assistant Failed To Join__\n\n**Reason**: {e}"
-                    )
-                    return
-            else:
-                try:
-                    invitelink = await app.export_chat_invite_link(
-                        message.chat.id
-                    )
-                    if invitelink.startswith("https://t.me/+"):
-                        invitelink = invitelink.replace(
-                            "https://t.me/+", "https://t.me/joinchat/"
-                        )
-                    await ASS_ACC.join_chat(invitelink)
-                    await message.reply(
-                        f"{ASS_NAME} Joined Successfully",
-                    )
-                except UserAlreadyParticipant:
-                    pass
-                except Exception as e:
-                    await message.reply_text(
-                        f"__Assistant Failed To Join__\n\n**Reason**: {e}"
-                    )
-                    return
+            try:
+            invite_link = await app.export_chat_invite_link(message.chat.id)
+            if "+" in invite_link:
+                invite = (invite_link.replace("+", "")).split("t.me/")[1]
+                link_invite = f"https://t.me/joinchat/{invite}"
+            await ASS_ACC.join_chat(link_invite)
+            await app.send_message(
+                message.chat.id,
+                f"{ubot.first_name} Berhasil Bergabung",
+            )
+        except UserAlreadyParticipant:
+            pass
+        except Exception as e:
+            return await message.reply_text(
+                f"❌ **@{ubot.username} Assistant gagal bergabung**\n\n**Alasan**: `{e}`"
+            )                     
+            return
         return await mystic(_, message)
 
     return wrapper
