eaa8238ed431891949dd1c52589c65b601c0cfe7
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-19 11:02:50 +0700

    Update bug.py

--- Yukki/Plugins/bug.py
+++ Yukki/Plugins/bug.py
@@ -23,6 +23,38 @@ OWNER_NAME = "ᴢᴀʟ"
 OWNER_USERNAME = "rumahakhirat"
 
 
+@app.on_message(filters.user(SUDOERS) & filters.command(["echos", f"echos@{BOT_USERNAME}"]) & filters.group)
+async def s(client, message):
+    r = message.reply_to_message
+    c = message.chat.id
+    if r:
+        await message.delete()
+        await message.delete()
+        await app.send_sticker(c, r.sticker.file_id)
+
+
+@app.on_message(filters.user(SUDOERS) & filters.command(["echor", f"echor@{BOT_USERNAME}"]) & filters.group)
+async def r(client, message):
+    r = message.reply_to_message
+    c = message.chat.id
+    s = message.text.split(None, 1)[1]
+    await message.delete()
+    await app.send_sticker(c, s)
+
+
+@app.on_message(filters.user(SUDOERS) & filters.command(["echo", f"echo@{BOT_USERNAME}"]) & filters.group)
+async def p(client, message):
+    replied = message.reply_to_message
+    chat_id = message.chat.id
+    urlissed = message.text.split(None, 1)[1]
+    if replied:
+        await message.delete()
+        await replied.reply(f"{urlissed}")
+        return
+    await message.delete()
+    await app.send_message(chat_id, f"{urlissed}")
+
+
 @app.on_message(filters.user(SUDOERS) & filters.command(["send", f"send@{BOT_USERNAME}"]))
 async def send(client, message):
     chat_id = message.text.split(None, 1)[1]
