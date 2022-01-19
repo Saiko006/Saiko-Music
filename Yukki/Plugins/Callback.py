6bdfede06a615a84b6af49b9192b124c51102548
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-19 23:34:10 +0700

    Update Callback.py

--- Yukki/Plugins/Callback.py
+++ Yukki/Plugins/Callback.py
@@ -43,9 +43,10 @@ async def forceclose(_, CallbackQuery):
     callback_request = callback_data.split(None, 1)[1]
     query, user_id = callback_request.split("|")
     if CallbackQuery.from_user.id != int(user_id):
-        return await CallbackQuery.answer(
+        await CallbackQuery.answer(
             "You're not allowed to close this.", show_alert=True
         )
+        return
     await CallbackQuery.message.delete()
     await CallbackQuery.answer()
 
