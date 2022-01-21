8bca3a84c48e0b995613284b8b214a2494e1df06
Izalmirage <94070031+Izalmirage@users.noreply.github.com>
2022-01-21 13:24:24 +0700

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -10,27 +10,27 @@ def PermissionCheck(mystic):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "I need to be admin with some permissions:\n"
-                + "\n- **can_manage_voice_chats:** To manage voice chats"
-                + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
-                + "\n- **can_invite_users**: For inviting assistant to chat."
+                "Saya perlu menjadi admin dengan beberapa izin:\n"
+                + "\n- **can_manage_voice_chats:** Untuk mengelola obrolan suara"
+                + "\n- **can_delete_messages:** Untuk menghapus Sampah yang Dicari Bot"
+                + "\n- **can_invite_users**: Untuk mengundang asisten."
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __MANAGE VOICE CHATS__"
+                "Saya tidak memiliki izin yang diperlukan untuk melakukan tindakan ini."
+                + "\n**Permission:** __MENGELOLA OBROLAN SUARA__"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __DELETE MESSAGES__"
+                "Saya tidak memiliki izin yang diperlukan untuk melakukan tindakan ini."
+                + "\n**Permission:** __HAPUS PESAN__"
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
                 "I don't have the required permission to perform this action."
-                + "\n**Permission:** __INVITE USERS VIA LINK__"
+                + "\n**Permission:** __UNDANG PENGGUNA DENGAN TAUTAN__"
             )
             return
         return await mystic(_, message)
